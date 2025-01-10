775a299bb36b23178cca8de49be7477e5bd03836
Subin Park <subinprk@student.42berlin.de>
2025-01-10 18:40:00 +0900

    adding header, modified CMakeLists.txt, but have to change the package name later cuz Local workspace has a package with same name

---
+++ src/lidar_processing/include/lidar_processing/lidar_processing.hpp
@@ -0,0 +1,21 @@
+#ifndef LIDAR_PROCESSING_LIDAR_PROCESSING_HPP
+# define LIDAR_PROCESSING_LIDAR_PROCESSING_HPP
+
+//Mandatories
+#include <ros/ros.h>
+
+//C++ libraries
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+//messages
+#include <sensor_msgs/PointCloud2.h>
+
+//PointCloud2
+#include <pcl/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+#endif
