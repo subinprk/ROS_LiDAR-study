fda16c2c7bab7e5dc41ae02efd6b5e29cd541b90
subinprk <subpark@student.42berlin.de>
2025-01-14 19:51:17 +0900

    the ransac has glitches but working

--- src/sb_lidar_process/src/main.cpp
+++ src/sb_lidar_process/src/main.cpp
@@ -7,7 +7,7 @@ ros::Publisher pub;
 void callback(const sensor_msgs::PointCloud2 &msg){
     pcl::PCLPointCloud2 pcl_pc;
     pcl_conversions::toPCL(msg, pcl_pc);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud(new pcl::PointCloud<pcl::PointXYZ>());
     pcl::fromPCLPointCloud2(pcl_pc, *input_cloud);
 
     ROI(input_cloud);
@@ -27,7 +27,7 @@ int main(int argc, char **argv){
     ros::init(argc, argv, "lidar_process_node");
     ros::NodeHandle nh;
 
-    ros::Subscriber sub = nh.subscribe("lidar3D", 5, callback); //1 is for Queuing the messages, have to make it higher in case processing takes longer time
+    ros::Subscriber sub = nh.subscribe("/lidar3D", 5, callback); //1 is for Queuing the messages, have to make it higher in case processing takes longer time
     pub = nh.advertise<obsts>("obsts", 5);
 
     ros::AsyncSpinner   spinner(2); //Two threads for concurrent callbacks
--- src/sb_lidar_process/src/planar_segmentation.cpp
+++ src/sb_lidar_process/src/planar_segmentation.cpp
@@ -1,12 +1,13 @@
 #include <sb_lidar_process/sb_lidar_process.hpp>
 
-void    ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr &input_cloud){
-    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>),
-                                        inlierPoints(new pcl::PointCloud<pcl::PointXYZ>);
+//sometimes the code has glitches
 
-    //Create the segmetation object
+void    ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr &input_cloud){
+    pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+    pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr inlierPoints(new pcl::PointCloud<pcl::PointXYZ>);
+
+    // Create the segmentation object
     pcl::SACSegmentation<pcl::PointXYZ> seg;
     seg.setModelType(pcl::SACMODEL_PLANE);
     seg.setMethodType(pcl::SAC_RANSAC);
@@ -15,12 +16,18 @@ void    ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr &input_cloud){
     seg.setInputCloud(input_cloud);
     seg.segment(*inliers, *coefficients);
 
-    pcl::copyPointCloud<pcl::PointXYZ>(*cloud, *inliers, *inlierPoints);
+    if (inliers->indices.empty()) {
+        std::cerr << "No inliers found!" << std::endl;
+        return;
+    }
+
+    // Now copy the inliers from input_cloud directly
+    pcl::copyPointCloud<pcl::PointXYZ>(*input_cloud, *inliers, *inlierPoints);
+
+    // Extract the inliers from the input cloud and store them in a new cloud
     pcl::ExtractIndices<pcl::PointXYZ> extract;
-    extract.setInputCloud(cloud);
+    extract.setInputCloud(input_cloud);
     extract.setIndices(inliers);
-    extract.setNegative(true);
+    extract.setNegative(true);  // Remove the inliers from the input cloud
     extract.filter(*input_cloud);
-    //filter (PCLPointCloud2 &output);
-    return ;
 }
