cbec236081a73aa7063fef0006d1cbed24332bda
subinprk <subpark@student.42berlin.de>
2025-01-15 18:58:36 +0900

    adding putting data into custom message

--- src/sb_lidar_process/src/clustering.cpp
+++ src/sb_lidar_process/src/clustering.cpp
@@ -20,21 +20,31 @@ void clustering(pcl::PointCloud<pcl::PointXYZ>::Ptr &input_cloud){
     ec.extract(cluster_indices);
 
     if (cluster_indices.empty()){
-        //std::cerr << "No clusters found" << std::endl;
+        std::cerr << "No clusters found" << std::endl;
         return ;
     }
-
-    //debugging
+    
+            //debugging
     pcl::PointCloud<pcl::PointXYZ>::Ptr centroids(new pcl::PointCloud<pcl::PointXYZ>);
-
+    std::vector<sb_lidar_process::obst> point_elem;
+    int count = 0;
     for (const auto &indices: cluster_indices){
         Eigen::Vector4f centroid;
         pcl::compute3DCentroid(*input_cloud, indices.indices, centroid);
         centroids->push_back(pcl::PointXYZ(centroid[0], centroid[1], centroid[2]));
+        sb_lidar_process::obst point;
+        point.x = centroid[0];
+        point.y = centroid[1];
+        point.z = centroid[2];
+        point_elem.push_back(point);
+        count ++;
     }
-    sensor_msgs::PointCloud2 output_msg;
-    pcl::toROSMsg(*centroids, output_msg);
-    output_msg.header.frame_id = "map";
-    output_msg.header.stamp = ros::Time::now();
+            sensor_msgs::PointCloud2 output_msg;
+            pcl::toROSMsg(*centroids, output_msg);
+            output_msg.header.frame_id = "map";
+            output_msg.header.stamp = ros::Time::now();
+    sb_lidar_process::obsts msg;
+    msg.count = count;
+    msg.obst_vector = point_elem;
     cluster_pub.publish(output_msg);
 }
